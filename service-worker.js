731bbab25b6e535683ef52d05e74119a9f591c9d
qazvac71-jpg <qazvac71@gmail.com>
2026-03-17 22:23:06 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'shop-kz-v1';
+const urlsToCache = [
+  '/shop.kz/',
+  '/shop.kz/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
