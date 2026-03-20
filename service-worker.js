e4ca2c7f843764d138e1e156833ec7272bfa14cd
qazvac71-jpg <qazvac71@gmail.com>
2026-03-20 23:14:38 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,72 @@
 const CACHE_NAME = 'shop-kz-v1';
-const urlsToCache = [
-  '/shop.kz/',
-  '/shop.kz/index.html'
+const STATIC_ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+// Установка - кэшируем статические ресурсы
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Кэширование статических ресурсов');
+                return cache.addAll(STATIC_ASSETS);
+            })
+            .then(() => self.skipWaiting())
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+// Активация - удаляем старые кэши
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+// Стратегия кэширования
+self.addEventListener('fetch', (event) => {
+    const { request } = event;
+    const url = new URL(request.url);
+
+    // Пропускаем Google Sheets API
+    if (url.hostname.includes('google.com')) {
+        return;
+    }
+
+    event.respondWith(
+        fetch(request)
+            .then((response) => {
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+                const responseToCache = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(request, responseToCache);
+                });
+                return response;
+            })
+            .catch(() => {
+                return caches.match(request).then((response) => {
+                    if (response) {
+                        return response;
+                    }
+                    return caches.match('/index.html');
+                });
+            })
+    );
 });
